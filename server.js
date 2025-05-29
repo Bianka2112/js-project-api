4cbcffb4bf56caa322b1c23bb90047c155d2157b
Bianka R <bianka_romero@me.com>
2025-05-30 00:40:09 +0200

    code endpoints  for collection of thoughts and single thought return

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import cors from "cors"
 import express from "express"
 
+import thoughtsList from "./data/thoughtsList.json"
+
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8000
@@ -12,7 +14,26 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  res.send("Hello !")
+})
+
+// Endpoint for all thought
+app.get("/thoughts", (req, res) => {
+  res.json(thoughtsList)
+})
+
+// Endpoint for one thought
+app.get("/thoughts/:id", (req, res) => {
+  
+  console.log("req.params", req.params.id)
+
+  const aThought = thoughtsList.find(t => t._id === req.params.id)
+
+  if (!aThought) {
+    return res.status(404).json({ error: "thought not found" })
+  }
+
+  res.json(aThought)
 })
 
 // Start the server
